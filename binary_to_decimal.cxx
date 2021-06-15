7b4b0a97c79aa2c5d8ff9ab05dfc123205f92a54
Shiva Silmawala <63483665+Shiva841@users.noreply.github.com>
2021-06-16 01:20:49 +0530

    Add files via upload

---
+++ binary_to_decimal.cxx
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n ;
+		cin>>n;
+	string s;
+	cin>>s;
+		int result =0;
+    int power2 =1;
+	for(int i=s.size()-1;i>=0;i--){
+		 int bd = s[i] -'0';
+		 result = result + (bd * power2);
+		 power2*=2;
+	}
+       cout<<result<<endl;
+	}
+}
